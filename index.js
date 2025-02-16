6359f93c5385572b36f62b5f69393223cdd9702a
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-02-16 21:38:25 +0530

    Set the socket.io

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require('http'); 
 const server = http.createServer(app);  
 const cors = require('cors'); 
+const { Socket } = require('socket.io');
 const io = require('socket.io')(server, {
     cors: {
         origin: '*',
@@ -18,6 +19,23 @@ const PORT = process.env.PORT || 5000;
 // Basic route
 app.get('/', (req, res) => {
     res.send('Server is running');
+});
+
+io.on('connection',(Socket)=>{
+    Socket.emit('me ',Socket.id);
+
+    Socket.on('disconnect',()=>{
+        Socket.broadcast.emit('callended');
+        console.log(`User ${Socket.id} disconnected`);
+    });
+
+    Socket.on('calluser',({userToCall,signalData,from,name})=>{
+        io.to(userToCall).emit('calluser',{signal:signalData,from,name});
+    });
+
+    Socket.on('answeruser',(data)=>{
+        io.to(data.to).emit('answeruser',data.signal);
+    });
 });
 
 // Start the server
